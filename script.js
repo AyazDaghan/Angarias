01fde923f594355c0f5335b4070d1b2f5f87ca7c
AyazDaghan <144120820+AyazDaghan@users.noreply.github.com>
2025-01-31 18:45:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function gosterSasirticiMesaj() {
     document.getElementById('sasirticiMesaj').style.display = 'block';
 }
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Belirlenen kullanıcı adı ve şifre
+    var correctUsername = "Angarias";
+    var correctPassword = "Luna"; // Örnek olarak "Luna" kullandım, istediğin şifreyi koyabilirsin.
+
+    if (username === correctUsername && password === correctPassword) {
+        window.location.href = "index.html"; // Başarılı giriş olursa yönlendir
+    } else {
+        document.getElementById("error-message").style.display = "block"; // Hata mesajı göster
+    }
+}
